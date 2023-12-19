cee85e66db3d8952a34930add2f8c8c95cd95fcb
sangeeth <your.email@example.com>
2023-12-19 10:07:02 +0530

    Order-Detail

--- src/main/java/com/example/layeredarchitecture/dao/OrderDAO.java
+++ src/main/java/com/example/layeredarchitecture/dao/OrderDAO.java
@@ -1,7 +1,11 @@
 package com.example.layeredarchitecture.dao;
 
+import com.example.layeredarchitecture.model.OrderDTO;
+
 import java.sql.SQLException;
 
 public interface OrderDAO {
-    String generateOrderId() throws SQLException, ClassNotFoundException ;
+    public String generateOID() throws SQLException, ClassNotFoundException;
+    public boolean existOrder(String orderId) throws SQLException, ClassNotFoundException;
+    public boolean saveOrder(OrderDTO dto) throws SQLException, ClassNotFoundException;
 }
